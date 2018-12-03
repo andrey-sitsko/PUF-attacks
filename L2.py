e817fd6fbe53143f1408fe0507f8750052262f82
Sitsko <a.sitsko@kino-mo.com>
2018-12-03 21:35:03 +0300

    add charts

--- L2.py
+++ L2.py
@@ -4,16 +4,21 @@ import pandas
 import numpy as np
 import os
 
-TRAINING_EDGE=0.9
-SAMPLES=['Base8.zip', 'Base16.zip', 'Base24.zip', 'Base32.zip', 'Base40.zip', 'Base96.zip']
+TRAINING_EDGE=0.7
+NROWS=200
+SAMPLES=['Base8.zip', 'Base16.zip', 'Base24.zip']
 
-#os.listdir('samples/Lab 2/') for all samples
-for fileName in SAMPLES:
+chartDataX = []
+chartDataY = []
+trainingEdge=0
+
+#for fileName in SAMPLES:
+for fileName in os.listdir('samples/Lab 2/'):
     name=os.path.splitext(fileName)[0]
     filepath=zipfile.ZipFile('./samples/Lab 2/{}'.format(fileName), 'r').extract('{}.txt'.format(name), 'Lab2_unzipped')
 
     logreg = LogisticRegression(C=1e5, solver='lbfgs', multi_class='multinomial')
-    dFrame=pandas.read_csv(filepath,sep=" ",header=None,dtype=str)
+    dFrame=pandas.read_csv(filepath,sep=" ",header=None,dtype=str,nrows=NROWS)
 
     X=[[int(c) for c in i] for i in dFrame.values[:,0]]
     Y=np.array(dFrame.values[:,1]).astype(int)
@@ -21,12 +26,15 @@ for fileName in SAMPLES:
     trainingEdge=int(len(dFrame.values) * TRAINING_EDGE)
 
     logreg.fit(X[:trainingEdge], Y[:trainingEdge])
-    print('Score for {} is {}'.format(name, logreg.score(X[trainingEdge:], Y[trainingEdge:])))
 
+    score=logreg.score(X[trainingEdge:], Y[trainingEdge:])
+    chartDataX.append(int(fileName.split("Base")[1].split(".zip")[0]))
+    chartDataY.append(int(score * 100))
 
+    print('Score for {} is {} training set length {}'.format(name, score, trainingEdge))
 
-
-
-
-
-
+import seaborn as sns
+import matplotlib.pyplot as plt
+chart=sns.regplot(x=chartDataX, y=chartDataY, fit_reg=False)
+chart.set_title("LogisticRegression, training set {}, test set {}".format(trainingEdge, NROWS - trainingEdge))
+plt.show()
